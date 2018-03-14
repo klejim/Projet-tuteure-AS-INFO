1a276e9cbac9f25c3f3cd944a89ebe8a98db5d4b
klejim <jimenezc@K118-4.dep-info.iutmontp.univ-montp2.fr>
2018-03-14 17:01:36 +0100

    Javadoc pour ConfigParser

--- src/project/network/ConfigParser.java
+++ src/project/network/ConfigParser.java
@@ -1,25 +1,31 @@
 package network;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.io.FileInputStream;
 
-
+/**
+ * 
+ * 
+ */
 public class ConfigParser {
-	private static final String FILENAME = "config.txt";
+	private static final String SECTION = "#{3} \\w+ #{3}";
 	private static final String VAR_DECLARATION = "\\w+ = (\\[|\\\")?.+(\\]|\\\")?";
-	public static HashMap<String, HashMap<String,Object>> parse()throws IOException{
+	private static final String NUMERIC = "[0-9]+";
+	private static final String STRING = "\"(.+ ?)+\"";
+	private static final String ARRAY = "\\[([0-9]+\\.?[0-9]?,? ?)+\\]";
+	public static HashMap<String, HashMap<String,Object>> parse(String filename)throws IOException{
 		HashMap<String,HashMap<String,Object>> config = new HashMap<>();
-		Scanner scan = new Scanner(new FileInputStream(FILENAME));
+		Scanner scan = new Scanner(new FileInputStream(filename));
 		String section = null;
 		HashMap<String,Object> vars = null;
 		while (scan.hasNext()){
 			String line = scan.nextLine();
-			if (line.matches("#{3} \\w+ #{3}")){
-				if (section != null && !config.containsKey(section)){
-					config.put(section,  vars);
+			if (line.matches(SECTION)){
+				if (section != null){
+					config.put(section, vars);
 				}
 				section = line.split(" ")[1];
 				vars = new HashMap<>();
@@ -32,15 +38,15 @@ public class ConfigParser {
 				 * chaîne de caractères
 				 * tableau de numériques
 				 */
-				if (expression.matches("[0-9]+")){ // 42
+				if (expression.matches(NUMERIC)){
 					Double value = Double.parseDouble(expression);
 					addToMap(vars, name, value);
 				}
-				else if (expression.matches("\"(\\w+ ?)+\"")){ // "chat"
+				else if (expression.matches(STRING)){
 					String value = expression.substring(1, expression.length()-1);
 					addToMap(vars, name, value);
 				}
-				else if (expression.matches("\\[([0-9]+\\.?[0-9]?,? ?)+\\]")){ // [12.0, 25,32]
+				else if (expression.matches(ARRAY)){
 					String tmp = expression.substring(1, expression.length()-1);
 					String vals[] = tmp.split(", ?");
 					ArrayList<Double> values = new ArrayList<>();
@@ -52,24 +58,24 @@ public class ConfigParser {
 				}
 			}
 		}
+		if (section != null){
+			config.put(section, vars); // dernière section
+		}
+		scan.close();
 		return config;
 	}
 	private static void addToMap(HashMap<String,Object> map, String key, Object value){
 		if (!map.containsKey(key)){
 			map.put(key, value);
-			
 		}
 	}
 	public static void main(String...args) throws IOException{
-		String test = "var = 12;";
-		System.out.println(test.matches(VAR_DECLARATION));
-		String line = "### DATA ###";
-		System.out.println(line.split(" ")[1]);
-		HashMap<String, HashMap<String,Object>> map = ConfigParser.parse();
+		HashMap<String, HashMap<String,Object>> map = ConfigParser.parse("config.txt");
 		for (String s : map.keySet()){
 			System.out.println("Section " + s);
+			HashMap<String,Object> configData = map.get(s);
 			for (String name : map.get(s).keySet()){
-				System.out.println(name + " = " + map.get(s).get(name));
+				System.out.println(name + " = " + configData.get(name));
 			}
 		}
 	}
