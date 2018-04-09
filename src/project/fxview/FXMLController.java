4807dcc1ca1f691766cf7bcfb1f2a9510fd9b990
klejim <clement.jimenez@outlook.com>
2018-04-09 13:30:22 +0200

    renomme package view en fxview

--- src/project/fxview/FXMLController.java
+++ src/project/fxview/FXMLController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package project.view;
+package project.fxview;
 
 import static java.awt.SystemColor.window;
 import java.net.URL;
