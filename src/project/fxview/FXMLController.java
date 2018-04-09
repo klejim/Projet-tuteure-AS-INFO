9ce2015bf00fa6f619ceab2c1366d7feafada924
klejim <jimenezc@K131-21.dep-info.iutmontp.univ-montp2.fr>
2018-04-09 16:33:55 +0200

    création de réseau et itérations basiques

--- src/project/fxview/FXMLController.java
+++ src/project/fxview/FXMLController.java
@@ -5,17 +5,19 @@
  */
 package project.fxview;
 
-import static java.awt.SystemColor.window;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import project.network.Network;
+import project.network.*;
 
 /**
  * FXML Controller class
@@ -27,14 +29,45 @@ public class FXMLController implements Initializable {
     private Stage stage;
     @FXML
     private AnchorPane anchor;
-    
+    @FXML
+    private TextArea test;
+    public String rapport() {
+        ArrayList<Node> unconnected = new ArrayList<>();
+        String str = "Eléments connectés : \n";
+        for (Node n : this.network.getNodes()) {
+            if (n instanceof SubStation) {
+                str += "" + n.getName() + "\n";
+                for (Line l : ((SubStation) n).getLines()) {
+                    str += "| <-- " + l.getIn().getName() + " " + l.getActivePower() + " kW\n";
+                }
+                for (Group g : ((SubStation) n).getGroups()) {
+                    str += "| --> " + g.getName() + " " + g.getConsumption() + " kW\n";
+                }
+                str += "Total IN: " + ((SubStation) n).getPowerIn() + " | Total OUT: " + ((SubStation) n).getPowerOut()
+                        + "\n";
+            } else if ((n instanceof Group || n instanceof PowerPlant) && !n.isConnected()) {
+                unconnected.add(n);
+            }
+        }
+        if (unconnected.size() > 0) {
+            str += "Eléments isolés : \n";
+            for (Node n : unconnected) {
+                str += n.getName() + "\n";
+            }
+        }
+        return str;
+    }
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
+     * @throws java.io.FileNotFoundException
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb){
         // TODO
+        
     }
     
     void setStage(Stage stge){
@@ -49,6 +82,24 @@ public class FXMLController implements Initializable {
         System.out.println("closing");
         // todo : avertissement et demande de sauvegarde
         Platform.exit();
+    }
+
+    @FXML
+    private void onIter(ActionEvent event) {
+        network.handleErrors(network.analyze());
+        network.update();
+	test.setText(rapport());
+    }
+
+    @FXML
+    private void onCreate(ActionEvent event) {
+        try {
+            network = new Network(0, 0, 0);
+            test.setText(rapport());
+        }catch (FileNotFoundException e){
+        }
+        
+            
     }
 
     
